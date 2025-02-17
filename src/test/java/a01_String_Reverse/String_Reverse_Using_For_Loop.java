b9d20baf8b8a4dabac72f680db7a35f21a90a9ef
Magesh <mageshtesting2022@gmail.com>
2025-02-17 17:33:29 +0530

    Practice_17_02_25

---
+++ src/test/java/a01_String_Reverse/String_Reverse_Using_For_Loop.java
@@ -0,0 +1,19 @@
+package a01_String_Reverse;
+
+public class String_Reverse_Using_For_Loop {
+
+	public static void main(String[] args) {
+
+		String phrase = "India is my country";
+		String lowerPhrase = phrase.toLowerCase();
+		String[] phraseArray = lowerPhrase.split(" ");
+		String reversedPhrase = " ";
+
+		for (int i = phraseArray.length - 1; i >= 0; i--) {
+			reversedPhrase = reversedPhrase + phraseArray[i] + " ";
+		}
+
+		System.out.println(reversedPhrase);
+	}
+
+}
---
+++ src/test/java/a03_Integer_Reverse/Integer_Reverse.java
@@ -0,0 +1,20 @@
+package a03_Integer_Reverse;
+
+public class Integer_Reverse {
+
+	public static void main(String[] args) {
+
+		int userInt = 221;
+		int reverse = 0;
+
+		while (userInt != 0) {
+			reverse = reverse * 10;
+			reverse = reverse + userInt % 10;
+			userInt = userInt / 10;
+		}
+
+		System.err.println("\nNumber after Swap: " + reverse);
+
+	}
+
+}
