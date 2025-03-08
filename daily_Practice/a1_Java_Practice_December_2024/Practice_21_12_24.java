fd35bebe40213cbcc558022661fa18ceae2f25f2
Magesh1999DDD <mageshyadav1999@gmail.com>
2025-03-08 05:36:24 +0530

    Folder_Changed_Update

--- daily_Practice/a1_Java_Practice_December_2024/Practice_21_12_24.java
+++
@@ -1,6 +0,0 @@
-package a1_Java_Practice_December_2024;
-
-public class Practice_21_12_24 {
-	
-
-}
---
+++ daily_Practice/a1_Java_Practice_March_2024/Practice_08_03_25.java
@@ -0,0 +1,8 @@
+package a1_Java_Practice_March_2024;
+
+public class Practice_08_03_25 {
+
+	public static void main(String[] args) {
+
+	}
+}
