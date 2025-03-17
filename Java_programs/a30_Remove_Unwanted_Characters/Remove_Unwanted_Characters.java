6e15daa160725d045a4df660dbe34c8afc91df8a
Magesh <mageshtesting2022@gmail.com>
2025-03-17 18:53:02 +0530

    Laptop_Practice_17_03_25

--- Java_programs/a30_Remove_Unwanted_Characters/Remove_Unwanted_Characters.java
+++ Java_programs/a30_Remove_Unwanted_Characters/Remove_Unwanted_Characters.java
@@ -5,11 +5,7 @@ public class Remove_Unwanted_Characters {
 	public static void main(String[] args) {
 
 		String name = "Magesh !@#$%&*(*&";
-
 		String replaceAll = name.replaceAll("[^a-zA-z0-9]", "");
-
 		System.out.println(replaceAll);
-
 	}
-
 }
