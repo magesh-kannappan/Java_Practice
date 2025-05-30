d18d89b930b5842ef0a10df6e4b0647632cff107
Magesh <mageshtesting2022@gmail.com>
2025-05-30 18:17:22 +0530

    program update push

--- Java_programs/a08_Even_Or_Odd/Even_Or_Odd.java
+++ Java_programs/a08_Even_Or_Odd/Even_Or_Odd.java
@@ -1,21 +1,25 @@
 package a08_Even_Or_Odd;
 
-public class Even_Or_Odd {
-
+// Program to check each digit of a number and print whether it's Even or Odd
+class Main {
 	public static void main(String[] args) {
 
-		int num = 14785;
+		int num = 12433;
 
+		// Loop until the number becomes 0
 		while (num > 0) {
+			// Get the last digit
+			int lastDigit = num % 10;
 
-			int rem = num % 10;
-
-			if (rem % 2 == 0) {
-				System.out.println("The number is even");
+			// Check if the digit is even or odd
+			if (lastDigit % 2 == 0) {
+				System.out.println(lastDigit + " is Even");
 			} else {
-				System.out.println("The number is odd");
+				System.out.println(lastDigit + " is Odd");
 			}
+
+			// Remove the last digit
+			num = num / 10;
 		}
 	}
-
 }
