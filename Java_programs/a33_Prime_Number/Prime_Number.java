f735876191ac176c82a17dbf4f2002f050a6a84a
Magesh <mageshtesting2022@gmail.com>
2025-06-13 18:21:25 +0530

    Laptop_Push_13_06_25

---
+++ Java_programs/a33_Prime_Number/Prime_Number.java
@@ -0,0 +1,25 @@
+package a33_Prime_Number;
+
+public class Prime_Number {
+
+	public static void main(String[] args) {
+
+		int num = 4; // Change this number to test others
+		boolean isPrime = true;
+
+		if (num <= 1) {
+			isPrime = false;
+		}
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		System.out.println(num + (isPrime ? " is a Prime Number" : " is NOT a Prime Number"));
+
+	}
+
+}
